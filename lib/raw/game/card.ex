19f1c3948a6cfd3e749fc6f506410ed2b307d11a
zodiake <ajaxsession@163.com>
2020-03-12 11:41:13 +0800

    add cards

--- lib/raw/game/card.ex
+++ lib/raw/game/card.ex
@@ -2,17 +2,46 @@ defmodule Raw.Game.Card do
   @moduledoc false
   alias __MODULE__
 
+  alias Raw.Game.{CardCompare, CardRuleSelector}
+
   @colors [:splade, :club, :hearts, :diamond]
+  @init_cards_num 17
   defstruct [:color, :value, :display_value]
 
   def new() do
-    for c <- @colors,
-        v <- 3..15 do
-      %Card{}
-      |> Map.put(:color, c)
-      |> Map.put(:value, v)
-      |> Map.put(:display_value, display_value(v))
-    end
+    big_joke = %Card{color: :big_joke, value: 16, display_value: "big_joke"}
+    little_joke = %Card{color: :little_joke, value: 17, display_value: "little_joke"}
+
+    cards =
+      for c <- @colors,
+          v <- 3..15 do
+        %Card{}
+        |> Map.put(:color, c)
+        |> Map.put(:value, to_string(v))
+        |> Map.put(:display_value, display_value(v))
+      end
+
+    [big_joke | [little_joke | cards]]
+  end
+
+  def cart_type(cards) do
+    CardRuleSelector.select_type(cards)
+  end
+
+  def shuffle(cards) do
+    Enum.shuffle(cards)
+  end
+
+  def deal_cards(cards) do
+    first_group = Enum.take(cards, @init_cards_num)
+    second_group = cards |> Enum.drop(@init_cards_num) |> Enum.take(@init_cards_num)
+    third_group = cards |> Enum.drop(@init_cards_num * 2) |> Enum.take(@init_cards_num)
+    landlord_group = cards |> Enum.drop(@init_cards_num * 3) |> Enum.take(3)
+    [first_group, second_group, third_group, landlord_group]
+  end
+
+  def sort(cards) do
+    Enum.sort(cards)
   end
 
   defp display_value(value) do
--- test/game/card_test.exs
+++ test/game/card_test.exs
@@ -5,6 +5,19 @@ defmodule Card.Game.CardTest do
 
   test "card new get 52 card" do
     cards = Card.new()
-    assert length(cards) == 52
+    assert length(cards) == 54
+  end
+
+  test "deal cards" do
+    cards = Card.new()
+
+    [f, s, t, left] =
+      cards
+      |> Card.shuffle()
+      |> Card.deal_cards()
+    assert length(f) == 17
+    assert length(s) == 17
+    assert length(t) == 17
+    assert length(left) == 3
   end
 end
