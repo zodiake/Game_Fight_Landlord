efd6d1b52aee18331b85545f2b3a2198e716206c
zodiake <ajaxsession@163.com>
2020-03-13 20:30:31 +0800

    change electing

--- test/game/game_test.exs
+++ test/game/game_test.exs
@@ -3,4 +3,9 @@ defmodule Raw.Game.GameTest do
   use ExUnit.Case
   alias Raw.Game.Game
 
+  test "game should empty when init" do
+    game = Game.start_link(%{guid: 1})
+    state = :sys.get_state(game)
+    assert state.game_id == 1
+  end
 end
